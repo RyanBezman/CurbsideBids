4f86debd1510961d1ec6faa1937426a6e62d8df1
Ryan Bezman <ryanbezman@icloud.com>
2026-01-28 18:32:41 -0500

    chagned UI to show users full name when signed in, instead of email. email is the fallback if no data is retrieved for users full name

--- App.tsx
+++ App.tsx
@@ -370,12 +370,14 @@ export default function App() {
             <View className="flex-row items-center">
               <View className="w-12 h-12 bg-violet-600 rounded-full items-center justify-center mr-4">
                 <Text className="text-white text-xl font-bold">
-                  {user.email?.charAt(0).toUpperCase()}
+                  {(user.user_metadata?.full_name || user.email)
+                    ?.charAt(0)
+                    .toUpperCase()}
                 </Text>
               </View>
               <View className="flex-1">
                 <Text className="text-white font-semibold text-base">
-                  {user.email}
+                  {user.user_metadata?.full_name || user.email}
                 </Text>
                 <Text className="text-neutral-500 text-sm">Rider</Text>
               </View>
